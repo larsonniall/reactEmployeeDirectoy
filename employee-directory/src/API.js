7d80e59ea95335172c2beb4410095cdb2a824319
Niall Larson <nialldizzle@gmail.com>
2020-08-15 11:47:52 -0700

    updated with less employees

--- employee-directory/src/API.js
+++ employee-directory/src/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default {
-    getUsers: function () {
+    getEmployees: function () {
         return axios.get("https://randomuser.me/api/?results=100&nat=us")
     }
 };
